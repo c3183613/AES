053e86f82adc01d4915c4616593d1f46b18ac12a
Jeremy Law <c3183613@uon.edu.au>
2016-05-20 11:46:25 +1000

    what do formatting

--- Application.java
+++ Application.java
@@ -1,113 +1,182 @@
-import java.util.List;
-public abstract class JAlgos
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class Application
 {
-	public static int[] rotWordRight(int[] wordIn)
+	// recieves two lines of binary input, prints the input 
+	// encrypts and prints encrypted plaintext, decrypts and prints plaintext
+	public static void main(String[] args) throws FileNotFoundException
 	{
-		int[] wordOut = wordIn;
-		int temp = wordOut[3];
-		for(int i = 3;i>0;i--)
+		String eOrD = args[0];
+		Scanner fileScanner = null;
+		try
 		{
-			wordOut[i] = wordOut[i-1];
+			fileScanner = new Scanner(new File(args[1]));
+
 		}
-		wordOut[0] = temp;
-		return wordOut;
-	}
-
-    // shift to left
-    // assumes input has 4 rows
-	public static void shiftRows(int[][] input)
-	{
-		// first row is untouched
-		//row 2 shift by 1
-		AES.rotWord(input[1]);
-		// row 3 shift by 2
-		AES.rotWord(input[2]);
-		AES.rotWord(input[2]);
-		//row 4 shift by 3
-		AES.rotWord(input[3]);
-		AES.rotWord(input[3]);
-		AES.rotWord(input[3]);
-	}
-
-	public static void shiftRowsInv(int[][] input)
-	{
-		// first row is untouched
-		// row 2 shift by 1
-		rotWordRight(input[1]);
-		// row 3 shift by 2
-		rotWordRight(input[2]);
-		rotWordRight(input[2]);
-		//row 4 shift by 3
-		rotWordRight(input[3]);
-		rotWordRight(input[3]);
-		rotWordRight(input[3]);
-	}
-
-	// intakes two dimension array and turns it into one dimensional array
-	public static int[] arrayMultiToOne(int[][] arrayParam)
-	{
-		int atIndex = 0;
-		int length = arrayParam.length;
-		int size = 0;
-		for(int i=0; i< length; i++)
+		catch(FileNotFoundException fnfe)
 		{
-			int innerlength = arrayParam[i].length;
-			for(int j=0; j<innerlength;j++)
+			System.out.println("File not found!");
+		}
+		finally
+		{
+			// take input
+			String binaryIn = fileScanner.nextLine();
+			String binaryInCopy = binaryIn;
+			// take key
+			String keyIn = fileScanner.nextLine();
+			String keyInCopy = keyIn;
+			// initialization 
+			int[][] input = new int[4][4];
+			int[][] key = new int[4][4];
+			// used to keep track of x and y values of 2d array
+			int l = 0, m = 0;
+			// do this for 16 bytes
+			for (int i = 0; i<16; i++) 
 			{
-				size++;
+				// System.out.println(l + " " + m);
+				String in = "";
+				// add the first character of binaryIn to 'in' and 
+				// remove it from original string
+				// 8 times for 8 bits = 1 byte
+				for (int j = 0; j<8; j++) 
+				{
+					in += binaryIn.charAt(0);
+					binaryIn = binaryIn.substring(1);
+				}
+				String keyWordIn = "";
+				// do the same for the key
+				for (int j = 0; j<8; j++) 
+				{
+					keyWordIn += keyIn.charAt(0);
+					keyIn = keyIn.substring(1);
+				}
+				// in is a binary number, turn it into a decimal number
+				input[l][m] = Integer.parseInt(in, 2);
+				key[l][m] = Integer.parseInt(keyWordIn, 2);
+				// keep track of which index we are at in the 2d array
+				if((i+1)%4 == 0 && i != 0)
+				{
+					m++;
+					l = 0;
+				}
+				else
+				{
+					l++;
+				}
 			}
-		}
-		@SuppressWarnings("unchecked")
-		int[] returnArray = new int[size];
-		for(int i=0; i<length; i++)
-		{
-			int innerlength = arrayParam[i].length;
-			for(int j=0; j<innerlength;j++)
+			ArrayList<int[][]> keyShedule = new ArrayList<int[][]>();
+			keyShedule = AES.keyExpansion(key);
+			PrintWriter writeToFile = null;
+			if(eOrD.equals("E") || eOrD.equals("e"))
 			{
-				returnArray[atIndex] =  arrayParam[i][j];
-				atIndex++;
+				ArrayList<int[][]> whatDoInput	 = AES.avFlipBit(binaryInCopy);
+				ArrayList<int[][]> whatDoKey	 = AES.avFlipBit(keyInCopy);
+				// Print out the hexadecimal representation of each array element in a cube
+				// long startTime = System.nanoTime();
+				long startTime = System.currentTimeMillis();
+				// encrypt the input using the key given
+				int[][] output = AES.encrypt(input, keyShedule);
+				long duration = (System.currentTimeMillis() - startTime);
+				ArrayList<ArrayList<String>> data = AES.generateAvalancheData(whatDoInput, whatDoKey);
+				float firstCount = 0;
+				float secondCount = 0;
+				ArrayList<Integer> aES0List = new ArrayList<Integer>();
+				ArrayList<Integer> aES1List = new ArrayList<Integer>();
+				ArrayList<Integer> aES2List = new ArrayList<Integer>();
+				ArrayList<Integer> aES3List = new ArrayList<Integer>();
+				ArrayList<Integer> aES4List = new ArrayList<Integer>();
+				ArrayList<Integer> aES0ListSecond = new ArrayList<Integer>();
+				ArrayList<Integer> aES1ListSecond = new ArrayList<Integer>();
+				ArrayList<Integer> aES2ListSecond = new ArrayList<Integer>();
+				ArrayList<Integer> aES3ListSecond = new ArrayList<Integer>();
+				ArrayList<Integer> aES4ListSecond = new ArrayList<Integer>();
+				for (int j = 0; j<55; j++) 
+				{
+					firstCount = secondCount = 0;
+					for (int i = 1; i<129; i++) 
+					{
+						firstCount += AES.bitsDifferent(data.get(j).get(0), data.get(j).get(i));
+					}
+					if(j < 11)
+						aES0List.add(new Integer(Math.round(firstCount/128)));
+					else if(11 <= j && j < 22)
+						aES1List.add(new Integer(Math.round(firstCount/128)));
+					else if(22 <= j && j < 33)
+						aES2List.add(new Integer(Math.round(firstCount/128)));
+					else if(33 <= j && j < 44)
+						aES3List.add(new Integer(Math.round(firstCount/128)));
+					else if(j < 55)
+						aES4List.add(new Integer(Math.round(firstCount/128)));
+					for (int i = 130; i<258; i++) 
+					{
+						secondCount += AES.bitsDifferent(data.get(j).get(0), data.get(j).get(i));
+					}
+					if(j < 11)
+						aES0ListSecond.add(new Integer(Math.round(secondCount/128)));
+					else if(11 <= j && j < 22)
+						aES1ListSecond.add(new Integer(Math.round(secondCount/128)));
+					else if(22 <= j && j < 33)
+						aES2ListSecond.add(new Integer(Math.round(secondCount/128)));
+					else if(33 <= j && j < 44)
+						aES3ListSecond.add(new Integer(Math.round(secondCount/128)));
+					else if(j < 55)
+						aES4ListSecond.add(new Integer(Math.round(secondCount/128)));
+				}
+				try
+				{
+					writeToFile = new PrintWriter("output.txt", "UTF-8");
+					writeToFile.println("ENCRYPTION");
+					writeToFile.println("Plaintext P: \t" + AES.matrixToString(input));
+					writeToFile.println("Key K: \t\t" + AES.matrixToString(key));
+					writeToFile.println("Ciphertext C: \t" + AES.matrixToString(output));
+					writeToFile.println("Running Time: \t"+duration+"ms");
+					writeToFile.println("Avalanche:\nP and Pi under K");
+					writeToFile.println("Round\t\tAES0\tAES1\tAES2\tAES3\tAES4");
+					for(int i = 0; i<aES0List.size();i++)
+					{
+						writeToFile.println("  " + i + "\t\t" + aES0List.get(i) + "\t" + aES1List.get(i) + "\t" + aES2List.get(i) + "\t" + aES3List.get(i) + "\t" + aES4List.get(i));
+					}
+					writeToFile.println("P under K and Ki");
+					writeToFile.println("Round\t\tAES0\tAES1\tAES2\tAES3\tAES4");
+					for(int i = 0; i<aES0List.size()-1;i++)
+					{
+						writeToFile.println("  " + i + "\t\t\t" + aES0ListSecond.get(i) + "\t" + aES1ListSecond.get(i) + "\t" + aES2ListSecond.get(i) + "\t" + aES3ListSecond.get(i) + "\t" + aES4ListSecond.get(i));
+					}
+					writeToFile.close();
+				}
+				catch(Exception e)
+				{
+					e.printStackTrace();
+				}
 			}
+			else if(eOrD.equals("d") || eOrD.equals("D"))
+			{
+				try
+				{
+					writeToFile = new PrintWriter("output.txt", "UTF-8");
+					int[][] decrypted = AES.decrypt(input, keyShedule);
+					writeToFile.println("DECRYPTION");
+					writeToFile.println("Ciphertext C: \t"+binaryInCopy);
+					writeToFile.println("Key K: \t\t"+keyInCopy);
+					writeToFile.println("Plaintext P: \t"+AES.matrixToString(decrypted));
+					writeToFile.close();
+				}
+				catch(Exception e)
+				{
+					e.printStackTrace();
+				}
+			}
+			else
+			{
+				System.out.println("Invalid input. Program is now exitting.");
+			}
+
 		}
-		return returnArray;
 	}
 
-	// Takes a size dimension array and turns it into a two dimensional array
-	// must specify is required of the two dimensional array
-	// Neither size1 or size2 may be 0
-	public static  int[][] arrayOneToMulti(int[] arrayParam, int size1, int size2) throws Exception
-	{
-		if(size1==0 || size2==0)
-		{
-			throw new Exception("Array cannot be of size 0");
-		}
-		@SuppressWarnings("unchecked")
-		int[][] returnArray = new int[size1][size2];
-		int atIndex =0;
-		int atIndexMulti1, atIndexMulti2; 
-		atIndexMulti1 = atIndexMulti2 = 0;
-		while(atIndex!=arrayParam.length)
-		{
-			returnArray[atIndexMulti1][atIndexMulti2] = arrayParam[atIndex];
-			if(++atIndexMulti2 == size2)
-			{
-				atIndexMulti1++;
-				atIndexMulti2 = 0;
-			}
-			atIndex++;
-		}
-		return returnArray;
-	}
-
-	public static String matrixToString(int[][] input)
-	{
-		String s ="";
-		for(int i=0;i<input.length;i++)
-		{
-			for(int j=0; j<input[i].length;j++)
-			{
-				s+=Integer.toBinaryString(input[j][i]);
-			}
-		}
-		return s;
-	}
+	
 }
---
+++ JAlgos.java
@@ -0,0 +1,113 @@
+import java.util.List;
+public abstract class JAlgos
+{
+	public static int[] rotWordRight(int[] wordIn)
+	{
+		int[] wordOut = wordIn;
+		int temp = wordOut[3];
+		for(int i = 3;i>0;i--)
+		{
+			wordOut[i] = wordOut[i-1];
+		}
+		wordOut[0] = temp;
+		return wordOut;
+	}
+
+    // shift to left
+    // assumes input has 4 rows
+	public static void shiftRows(int[][] input)
+	{
+		// first row is untouched
+		//row 2 shift by 1
+		AES.rotWord(input[1]);
+		// row 3 shift by 2
+		AES.rotWord(input[2]);
+		AES.rotWord(input[2]);
+		//row 4 shift by 3
+		AES.rotWord(input[3]);
+		AES.rotWord(input[3]);
+		AES.rotWord(input[3]);
+	}
+
+	public static void shiftRowsInv(int[][] input)
+	{
+		// first row is untouched
+		// row 2 shift by 1
+		rotWordRight(input[1]);
+		// row 3 shift by 2
+		rotWordRight(input[2]);
+		rotWordRight(input[2]);
+		//row 4 shift by 3
+		rotWordRight(input[3]);
+		rotWordRight(input[3]);
+		rotWordRight(input[3]);
+	}
+
+	// intakes two dimension array and turns it into one dimensional array
+	public static int[] arrayMultiToOne(int[][] arrayParam)
+	{
+		int atIndex = 0;
+		int length = arrayParam.length;
+		int size = 0;
+		for(int i=0; i< length; i++)
+		{
+			int innerlength = arrayParam[i].length;
+			for(int j=0; j<innerlength;j++)
+			{
+				size++;
+			}
+		}
+		@SuppressWarnings("unchecked")
+		int[] returnArray = new int[size];
+		for(int i=0; i<length; i++)
+		{
+			int innerlength = arrayParam[i].length;
+			for(int j=0; j<innerlength;j++)
+			{
+				returnArray[atIndex] =  arrayParam[i][j];
+				atIndex++;
+			}
+		}
+		return returnArray;
+	}
+
+	// Takes a size dimension array and turns it into a two dimensional array
+	// must specify is required of the two dimensional array
+	// Neither size1 or size2 may be 0
+	public static  int[][] arrayOneToMulti(int[] arrayParam, int size1, int size2) throws Exception
+	{
+		if(size1==0 || size2==0)
+		{
+			throw new Exception("Array cannot be of size 0");
+		}
+		@SuppressWarnings("unchecked")
+		int[][] returnArray = new int[size1][size2];
+		int atIndex =0;
+		int atIndexMulti1, atIndexMulti2; 
+		atIndexMulti1 = atIndexMulti2 = 0;
+		while(atIndex!=arrayParam.length)
+		{
+			returnArray[atIndexMulti1][atIndexMulti2] = arrayParam[atIndex];
+			if(++atIndexMulti2 == size2)
+			{
+				atIndexMulti1++;
+				atIndexMulti2 = 0;
+			}
+			atIndex++;
+		}
+		return returnArray;
+	}
+
+	public static String matrixToString(int[][] input)
+	{
+		String s ="";
+		for(int i=0;i<input.length;i++)
+		{
+			for(int j=0; j<input[i].length;j++)
+			{
+				s+=Integer.toBinaryString(input[j][i]);
+			}
+		}
+		return s;
+	}
+}
